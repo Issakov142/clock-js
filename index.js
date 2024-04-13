dc7aab6a7d07435529cf2475f5f53abdfea35ad0
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-04-13 23:10:17 +0500

    double click bug fixed

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let timerId; // переменная, которая будет хранить I
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function () {
   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-  timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+  if (!timerId) {
+    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+  }
 });
 
 const stopButton = document.getElementById("stop");
